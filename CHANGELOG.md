9b4927ecd4b69f16d822f9845c1e1429cb7a0b6e
Victoriano Diaz <vd@precisiondata.com.br>
2023-01-27 17:00:10 -0300

    Project organization and preparing first feature: size calcuation

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## v0.1.0
+- Size calculation of folders and containers
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='azure-datalake-commons',
+    version='0.1.0',
+    url='https://github.com/alfonso777/azure-datalake-commons',
+    author='Victoriano Diaz',
+    author_email='',
+    description='Azure Datalake common utilities',
+    #packages=find_packages(where='src'),
+    packages=find_packages(),
+    install_requires=[]
+)
